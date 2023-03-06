aa974bbb641cc2d168a28697be1a90c02db13861
AnClark <clarklaw4701@qq.com>
2023-03-06 11:54:38 +0800

    casynth: Only activate DPF UI macros when available

--- plugins/casynth/DistrhoPluginInfo.h
+++ plugins/casynth/DistrhoPluginInfo.h
@@ -6,9 +6,11 @@
 #define DISTRHO_PLUGIN_URI "https://github.com/AnClark/infamousPlugins.git"
 #define DISTRHO_PLUGIN_CLAP_ID "infamousPlugins.casynth"
 
+#ifdef ENABLE_UI
 #define DISTRHO_PLUGIN_HAS_UI 1
 #define DISTRHO_PLUGIN_HAS_EMBED_UI 1
 #define DISTRHO_PLUGIN_HAS_EXTERNAL_UI 1
+#endif
 #define DISTRHO_PLUGIN_IS_RT_SAFE 1
 #define DISTRHO_PLUGIN_IS_SYNTH 1
 #define DISTRHO_PLUGIN_NUM_INPUTS 1

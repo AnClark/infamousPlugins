d87153c0f31b2d709ce75fd8723eabd3f2236412
AnClark Liu <anclarkliu@outlook.com>
2023-10-30 10:41:35 +0800

    build: cairo: Fix static link error
    By default, if you link static libraries (FLTK, DPF plugin) to Cairo,
    linker will report a bunch of error of "undefined reference to
    __imp_cairo*". It only happens on Win32.
    
    This is because Cairo does not encourage static linkage on Win32
    platform. To explicitly enable Win32 static build, we need to define
    macro CAIRO_WIN32_STATIC_BUILD, globally.

--- cmake/BuildCairo.cmake
+++ cmake/BuildCairo.cmake
@@ -52,3 +52,13 @@ set (CAIRO_FOUND ON)
 set (CAIRO_LIBRARIES cairo pixman-1)
 set (CAIRO_LIBRARY_DIRS ${DEPS_PREFIX_PATH}/lib)
 set (CAIRO_INCLUDE_DIRS ${DEPS_PREFIX_PATH}/include/cairo)
+
+# To link against static Cairo on Win32 platform, make sure you've defined macro CAIRO_WIN32_STATIC_BUILD.
+# Otherwise, plenty of "undefined reference to __impl_cairo_*" linker error will occur!
+# See:
+#   - https://cairo.cairographics.narkive.com/NRTD7KEw/win32-static-linking
+#   - https://gitlab.freedesktop.org/cairo/cairo/-/merge_requests/265
+#   - https://cgit.freedesktop.org/cairo/tree/src/win32/cairo-win32-system.c
+if (WIN32)
+    add_compile_definitions (CAIRO_WIN32_STATIC_BUILD=1)
+endif ()
